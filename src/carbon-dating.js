9c492faa3363596487f02b58e73253f61e3e37db
Tati-Zhurr <zhtv88@gmail.com>
2022-09-02 17:35:32 +0200

    add solution for count-cat and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if ((typeof(sampleActivity) !== 'string')||(sampleActivity*1 > MODERN_ACTIVITY)||(sampleActivity*1 <= 0)){
+    return false;
+  };
+  
+ let k = 0.693/HALF_LIFE_PERIOD;
+ let age =Math.ceil(Math.log(MODERN_ACTIVITY/(sampleActivity*1))/k);
+ console.log(age);
+ return (age)&&(age > 0)? age: false;
 }
 
 module.exports = {
